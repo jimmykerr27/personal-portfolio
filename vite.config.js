fcc11836e735e1ce6cad9197536dcfed0d9e57c8
jimmykerr27 <jamesrkerr27@gmail.com>
2025-07-30 21:27:10 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
-    },
-  },
-  base: "/james-kerr-portfolio/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
