a2c5e1a41797a428c9c4ce53bb64c02bc7354275
jimmykerr27 <126014762+jimmykerr27@users.noreply.github.com>
2025-08-02 17:30:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter basename="/personal-portfolio">
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path='*' element={<NotFound />} />
